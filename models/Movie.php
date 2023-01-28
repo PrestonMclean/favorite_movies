a05e279ca44d160599f083690105f50ac690e541
Preston Mclean <mclean_preston@hotmail.com>
2023-01-28 01:40:14 +0000

    allow rating to be seen and add the search_by_genre function

--- models/Movie.php
+++ models/Movie.php
@@ -6,6 +6,8 @@
         public $id;
         public $title;
         public $description;
+        public $rating;
+        public $genre;
 
         public function __construct($db) {
             $this->conn = $db;
@@ -20,25 +22,51 @@
         }
 
         public function read_single() {
-            $query = 'SELECT * FROM ' . $this->table . ' WHERE id =? LIMIT 0,1';
+            $this->genre = array();
+            $query = 'SELECT m.*, g.name
+                FROM ' . $this->table . ' m
+	            JOIN movies_genres mg ON m.id = mg.movie_id
+                JOIN genres g ON mg.genre_id = g.id
+                WHERE m.id = ?';
 
             $stmt = $this->conn->prepare($query);
 
             $stmt->bindParam(1, $this->id);
 
             $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            $this->id = $row['id'];
-            $this->title = $row['title'];
-            $this->description = $row['description'];
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                array_push($this->genre, $name);
+            }
+            $this->id = $id;
+            $this->title = $title;
+            $this->description = $description;
+            $this->rating = $rating;
+
+        }
+
+        public function search_by_genre()
+        {
+            $query = 'SELECT m.*, g.name
+                FROM ' . $this->table . ' m
+	            JOIN movies_genres mg ON m.id = mg.movie_id
+                JOIN genres g ON mg.genre_id = g.id
+                WHERE g.name = ?';
+
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->bindParam(1, $this->genre);
+            $stmt->execute();
+            return $stmt;
         }
 
         public function create() {
             $query = 'INSERT INTO ' . $this->table . '
                 SET
                     title = :title,
-                    description = :description
+                    description = :description,
+                    rating = :rating
             ';
 
             $stmt = $this->conn->prepare($query);
@@ -48,6 +76,7 @@
 
             $stmt->bindParam(':title', $this->title);
             $stmt->bindParam(':description', $this->description);
+            $stmt->bindParam(':rating', $this->rating);
 
             if ($stmt->execute()) {
                 return true;
